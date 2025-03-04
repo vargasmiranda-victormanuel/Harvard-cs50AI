e8f5412102efccc01cde15725332f8d84dce7be5
vargasmiranda-victormanuel <victorvargasmiranda@gmail.com>
2025-03-04 14:00:48 -0600

    Add Degrees Project with an answer

--- Projects/00-Degrees/degrees.py
+++ Projects/00-Degrees/degrees.py
@@ -92,9 +92,54 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # Keep track of number of states explored
+    num_explored = 0
 
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
+    while True:
+
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            return None
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+        num_explored += 1
+
+        # If node is the goal, then we have a solution
+        if node.state == target:
+            print("solution found in loop: ", num_explored)
+            return is_solved(node)
+
+        # Mark node as explored
+        explored.add(node.state)
+
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(node.state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
+
+                if child.state == target:
+                    print("solution found in loop: ", num_explored)
+                    return is_solved(child)
+
+def is_solved(node):
+    solution = []
+    while node.parent is not None:
+        person = (node.action, node.state)
+        solution.append(person)
+        node = node.parent
+    solution.reverse()
+    return solution
 
 def person_id_for_name(name):
     """
